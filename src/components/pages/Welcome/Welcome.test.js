e2bc173b7157b5d2b5e4899f3194b1bd4fbd1329
stynan27 <seamustynan16@gmail.com>
2023-06-27 17:18:11 -0400

    #14 Adjust Welcome page & tests

--- src/components/pages/Welcome/Welcome.test.js
+++ src/components/pages/Welcome/Welcome.test.js
@@ -4,10 +4,11 @@ import { WelcomePage } from '.';
 // These unit tests were based on the acceptance criteria for the Welcome Page User Story.
 test('renders Welcome Page component', () => {
     // Arrange
+    const expectedTestId = 'welcome-page';
 
     // Act
     const { getByTestId } = render(<WelcomePage />);
-    const welcomePage = getByTestId('welcome-page');
+    const welcomePage = getByTestId(expectedTestId);
 
     // Assert
     expect(welcomePage).toBeInTheDocument();
@@ -16,10 +17,11 @@ test('renders Welcome Page component', () => {
 
 test('renders Welcome Page title', () => {
     // Arrange
+    const expectedRegex = /Welcome to BlockHeads!/i;
 
     // Act
     render(<WelcomePage />);
-    const carouselCaptionTitles = screen.getAllByText(/Welcome to BlockHeads!/i);
+    const carouselCaptionTitles = screen.getAllByText(expectedRegex);
 
     // Assert
     expect(carouselCaptionTitles[0]).toBeInTheDocument();
@@ -27,12 +29,15 @@ test('renders Welcome Page title', () => {
 
 test('renders Welcome Page description', () => {
     // Arrange
-    const expectedSubtring = "BlockHeads was designed to be an all-in-one solution to Lego set management"
+    const expectedSubtring = "BlockHeads was designed to be an all-in-one solution to Lego set management";
+    // "-0" here provided by id in component
+    const expectedTestId = 'welcome-description-0';
 
     // Act
     const { getByTestId } = render(<WelcomePage />);
+    const welcomeDescriptionTag = getByTestId(expectedTestId);
     
     // Assert
-    expect(getByTestId('welcome-description-0')).toBeInTheDocument();
-    expect(getByTestId('welcome-description-0').textContent).toContain(expectedSubtring);
+    expect(welcomeDescriptionTag).toBeInTheDocument();
+    expect(welcomeDescriptionTag.textContent).toContain(expectedSubtring);
 });
--- src/components/pages/Welcome/index.jsx
+++ src/components/pages/Welcome/index.jsx
@@ -19,15 +19,15 @@ export function WelcomePage() {
             pause={false} 
             fade={true}
         >
-            {imageList.map((image, idx) => (
-                <Carousel.Item key={idx}>
+            {imageList.map((image, id) => (
+                <Carousel.Item key={id}>
                     <Carousel.Caption className='welcome-caption lego-regular'>
                         <Container>
                             <Row>
                                 <Col></Col>
                                 <Col sm={6} className='caption-col'>
                                     <h2>Welcome to BlockHeads!</h2>
-                                    <p data-testid={"welcome-description-" + idx}>
+                                    <p data-testid={"welcome-description-" + id}>
                                         BlockHeads was designed to be an all-in-one solution to Lego set management. 
                                         See how we can build your digital collection today...
                                     </p>
