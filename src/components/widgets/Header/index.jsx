e4241bed9bcbca8239cb4a3fcd650c3c4dd780ea
= <=>
2023-06-28 14:44:30 -0400

    #12 - relative path to shadow.png asset & test btn

--- src/components/widgets/Header/index.jsx
+++ src/components/widgets/Header/index.jsx
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Button, Image} from 'react-bootstrap';
 import{Github} from 'react-bootstrap-icons';
-import headerLogo from '/home/jtangelo98/dev/BlockHeads/src/assets/shadow.png';
+import headerLogo from '../../../assets/shadow.png';
 
 //import './style.css';
 
@@ -13,15 +13,25 @@ export function HeaderComponent() {
         >
             <Row>
                 <Col>
-                    <Button className='blockheadsImageButton px-0 py-0 border-0'><Image className = 'mx-0 my-0' src = {headerLogo}  style = {{width: 250, verticalAlign: "top", paddingTop: 0}} alt = "BlockHeads Logo"/>  
+                    <Button 
+                        className='blockheadsImageButton px-0 py-0 border-0'
+                    >
+                        <Image 
+                            className='mx-0 my-0'
+                            src={headerLogo}
+                            style={{width: 250, verticalAlign: "top", paddingTop: 0}}
+                            alt="BlockHeads Logo"
+                        />  
                     </Button>
                     <Button variant = "dark" className = 'blockheadsImageButton rounded-circle px-0 py-0' size = "sm" style = {{color: 'black'}}>
                         <Github size = {50}/>
                     </Button>
                 </Col> 
-                {/* <Col>
-                    <Button style = {{height: 45, width: 120, paddingTop: 15}}/>
-                </Col>  */}
+                <Col>
+                    <Button>
+                        Test
+                    </Button>
+                </Col>
             </Row> 
 
         </Container>
