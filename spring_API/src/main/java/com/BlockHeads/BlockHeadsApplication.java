8d7d4d2ea489bf5a26f065017feaed367f94694e
stynan27 <seamustynan16@gmail.com>
2023-06-28 21:18:50 -0400

    #18 init Spring API

---
+++ spring_API/src/main/java/com/BlockHeads/BlockHeadsApplication.java
@@ -0,0 +1,13 @@
+package com.BlockHeads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockHeadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockHeadsApplication.class, args);
+	}
+
+}
---
+++ spring_API/src/test/java/com/BlockHeads/BlockHeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlockHeads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockHeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
