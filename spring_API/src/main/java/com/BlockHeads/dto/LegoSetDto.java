d8d5ba9355782b0f0b0fe436b0e3c3725965bb46
stynan27 <seamustynan16@gmail.com>
2023-09-17 18:32:38 -0400

    #26 New LegoSet and DTO types

---
+++ spring_API/src/main/java/com/BlockHeads/dto/LegoSetDto.java
@@ -0,0 +1,43 @@
+package com.BlockHeads.dto;
+
+import com.BlockHeads.model.LegoSet;
+
+public class LegoSetDto extends LegoSet {
+	
+	private String errorMessage;
+	
+
+	public LegoSetDto() {
+		super();
+	}
+
+
+	public LegoSetDto(LegoSet legoSet, String errorMessage) {
+		super(legoSet.getId(), legoSet.getUserAccount(), legoSet.getName(), 
+				legoSet.getIdentifyingNumber(), legoSet.getDescription(), 
+				legoSet.getNumberOfPieces(), legoSet.getPrice(), legoSet.getInstructions());
+		this.errorMessage = errorMessage;
+	}
+
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+
+	@Override
+	public String toString() {
+		return "LegoSetDto [errorMessage=" + errorMessage + ", getUserAccount()=" + getUserAccount() + ", getName()="
+				+ getName() + ", getIdentifyingNumber()=" + getIdentifyingNumber() + ", getDescription()="
+				+ getDescription() + ", getNumberOfPieces()=" + getNumberOfPieces() + ", getPrice()=" + getPrice()
+				+ ", getInstructions()=" + getInstructions() + ", toString()=" + super.toString() + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + "]";
+	}
+	
+	
+}
---
+++ spring_API/src/main/java/com/BlockHeads/model/LegoSet.java
@@ -0,0 +1,121 @@
+package com.BlockHeads.model;
+
+import java.sql.Blob;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class LegoSet {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserAccount userAccount;
+    
+    private String name;
+    private Integer identifyingNumber;
+    private String description;
+    private Integer numberOfPieces;
+    private Float price;
+    
+    @Lob
+    private Blob instructions;
+
+	public LegoSet() {
+		super();
+	}
+
+	public LegoSet(Integer id, UserAccount userAccount, String name, Integer identifyingNumber, String description,
+			Integer numberOfPieces, Float price, Blob instructions) {
+		super();
+		this.id = id;
+		this.userAccount = userAccount;
+		this.name = name;
+		this.identifyingNumber = identifyingNumber;
+		this.description = description;
+		this.numberOfPieces = numberOfPieces;
+		this.price = price;
+		this.instructions = instructions;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Integer getIdentifyingNumber() {
+		return identifyingNumber;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Integer getNumberOfPieces() {
+		return numberOfPieces;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public Blob getInstructions() {
+		return instructions;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public void setUserAccount(UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setIdentifyingNumber(Integer identifyingNumber) {
+		this.identifyingNumber = identifyingNumber;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setNumberOfPieces(Integer numberOfPieces) {
+		this.numberOfPieces = numberOfPieces;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public void setInstructions(Blob instructions) {
+		this.instructions = instructions;
+	}
+
+	@Override
+	public String toString() {
+		return "LegoSet [id=" + id + ", userAccount=" + userAccount + ", name=" + name + ", identifyingNumber="
+				+ identifyingNumber + ", description=" + description + ", numberOfPieces=" + numberOfPieces + ", price="
+				+ price + ", instructions=" + instructions + "]";
+	}
+    
+    
+}
