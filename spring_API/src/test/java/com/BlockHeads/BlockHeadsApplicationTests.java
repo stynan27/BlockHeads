669e08da890e90f3a6f252971ce8d236bdad572f
stynan27 <seamustynan16@gmail.com>
2023-10-08 14:55:01 -0400

    #19 & #44 - setup for API tests

--- spring_API/src/test/java/com/BlockHeads/BlockHeadsApplicationTests.java
+++ spring_API/src/test/java/com/BlockHeads/BlockHeadsApplicationTests.java
@@ -1,13 +1,77 @@
 package com.BlockHeads;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import com.BlockHeads.model.UserAccount;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BlockHeadsApplicationTests {
 
+//    private static String userAccountBaseUrl;
+//    private static URI userAccountRegisterUri;
+//    private static URI userAccountLoginUri;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    // TODO: Fix - the following needs to be static to use @BeforeAll, 
+    // however that prevents us from using port here as well...
+    @BeforeAll
+    public static void setup() {
+//		try {
+//			userAccountBaseUrl = "http://localhost:" + port + "/api/user";
+//	        userAccountRegisterUri = new URI(userAccountBaseUrl + "/register");
+//	        userAccountLoginUri = new URI(userAccountBaseUrl + "/login");
+//		} catch (URISyntaxException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+    }
+    
 	@Test
 	void contextLoads() {
 	}
+	
+    @Test
+    public void testCreateUserReturnsCreated() {
+    	// TODO: Local test workaround for the URI (should be defined in @BeforeAll method)
+    	String userAccountBaseUrl = "http://localhost:" + port + "/api/user";
+    	URI userAccountRegisterUri = null;
+		try {
+			userAccountRegisterUri = new URI(userAccountBaseUrl + "/register");
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	// Arrange
+		// TODO: Since these tests re-use our local database, 
+		// will need to perform cleanup steps to ensure the test user/sets are removed!!!
+        UserAccount testUserAccount = new UserAccount();
+        testUserAccount.setId(1);
+        testUserAccount.setUsername("stynan");
+        testUserAccount.setPassword("password123");
+        
+        // Execute
+        HttpStatus createStatus = (HttpStatus) restTemplate.postForEntity(userAccountRegisterUri, testUserAccount, UserAccount.class).getStatusCode();
+
+        // Assert
+        assertEquals(HttpStatus.CREATED, createStatus);
+    }
 
 }
