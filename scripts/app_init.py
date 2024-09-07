cba0a7f202c582bd09a01e9ba3d711f038c518a4
moose <seamustynan16@gmail.com>
2024-09-07 15:30:27 -0400

    #46 - migrate error text and COMMANDS dict

--- scripts/app_init.py
+++ scripts/app_init.py
@@ -10,14 +10,15 @@ PROJECT_ROOT = Path(__file__).absolute().parent.parent # Blockheads project root
 REACT_CLIENT_PATH = str(PROJECT_ROOT / 'react_client')
 SPRING_API_PATH = str(PROJECT_ROOT / 'spring_API')
 
-# COMMANDS
-NPM_RUN_START = [ 
-    'gnome-terminal', # Launches new gnome-terminal for output logging
-    '--', 
-    'bash', 
-    '-c', 
-    'npm run start'
-] 
+COMMANDS: dict = {
+    "NPM_RUN_START": [ 
+        'gnome-terminal', # Launches new gnome-terminal for output logging
+        '--', 
+        'bash', 
+        '-c', 
+        'npm run start'
+    ]
+}
 
 def handle_result(cmd, cwd, res):
     cmd_str = str(cmd)
@@ -30,6 +31,11 @@ def handle_result(cmd, cwd, res):
             print('')
             return True
         print("Error: " + res.stderr)
+        
+        # https://github.com/ros2/ros2/issues/1406
+        print('\nNOTE - If received the following error:')
+        print(GLIBC_PRIVATE_ERROR)
+        print('Run: "unset GTK_PATH" command first\n\n')
         return False
     else:
         print("Message: " + res.stdout)
@@ -39,12 +45,13 @@ def handle_result(cmd, cwd, res):
 # TODO: method to run react app
 def run_react_client():
     print('Launching Blockheads React client')
-    res = subprocess.run(NPM_RUN_START, \
+    cmd = COMMANDS['NPM_RUN_START']
+    res = subprocess.run(cmd, \
             cwd=REACT_CLIENT_PATH, \
             capture_output=True, \
             text=True
         )
-    return handle_result(NPM_RUN_START, REACT_CLIENT_PATH, res)
+    return handle_result(cmd, REACT_CLIENT_PATH, res)
 
 
 # TODO: method to run java app
@@ -53,13 +60,13 @@ def run_react_client():
 
 # TODO: method to populate MySQL with init.sql data
 
+
 if __name__ == '__main__':
-    # https://github.com/ros2/ros2/issues/1406
-    print('NOTE - If received the following error:')
-    print(GLIBC_PRIVATE_ERROR)
-    print('Run: "unset GTK_PATH" command first\n\n')
+    
+    
     
     if not run_react_client():
         print('React client failed to launch.')
         raise SystemExit(1)
+    
     print('Blockheads is up and running!')
