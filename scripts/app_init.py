e7299907398375880e2fa00c05de84ec8eb3bb73
moose <seamustynan16@gmail.com>
2024-09-11 20:42:29 -0400

    #46 - running MySQL

--- scripts/app_init.py
+++ scripts/app_init.py
@@ -3,9 +3,11 @@ import time
 from datetime import datetime
 from pathlib import Path
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 ### GLOBAL SCOPED VARS
-SECONDS_BETWEEN_COMMANDS = 10
+MAX_TERMINAL_LAUNCH_SECONDS = 5
+MAX_CONSECUTIVE_FWRITES_SECONDS = 10
 BLOCKHEADS_JAR_NAME = 'BlockHeads-0.0.1-SNAPSHOT.jar'
 TMP_BACKEND_OUTPUT_FILE_NAME = 'app_init_backend_output.txt'
 TMP_REACT_OUTPUT_FILE_NAME = 'app_init_react_output.txt'
@@ -36,8 +38,26 @@ COMMANDS: dict = {
         '/tmp/' + TMP_REACT_OUTPUT_FILE_NAME + ';',
         "bash" # keep terminal open
     ],
-    "AND_IF": ['&&'], # chain commands, unless they error
-    "SLEEP": ['sleep', str(SECONDS_BETWEEN_COMMANDS)], # wait specified number of seconds
+    "DOCKER_PS": ['docker', 'ps'],
+    "DOCKER_RUN_MYSQL": [
+        'docker', 
+        'run',
+        '--detach',
+        '--restart=always',
+        '--env',
+        'MYSQL_ROOT_PASSWORD=dummypassword',
+        '--env',
+        'MYSQL_USER=blockheads-developer',
+        '--env',
+        'MYSQL_PASSWORD=dummypassword',
+        '--env',
+        'MYSQL_DATABASE=blockheads-database',
+        '--name',
+        'blockheads-mysql',
+        '--publish',
+        '3306:3306',
+        'mysql:8-oracle'
+    ],
     "CLEAN_BACKEND_OUTPUT": ['rm','-f', TMP_BACKEND_OUTPUT_FILE_NAME],
     "CLEAN_REACT_OUTPUT": ['rm','-f', TMP_REACT_OUTPUT_FILE_NAME],
     "CLEAN_COMPILE": ['rm','-rf','target/'],
@@ -48,6 +68,8 @@ COMMANDS: dict = {
 }
 
 def setup():
+    print('Running app setup\n')
+    
     if not run_command_no_terminal(COMMANDS['CLEAN_BACKEND_OUTPUT'], '/tmp/'):
         return False
     
@@ -58,20 +80,24 @@ def setup():
     
 
 # generic method to handle method returns on error vs output
-def handle_result(cmd, cwd, res=None, stdout='', stderr=''):
-    cmd_str = str(cmd)
-    
-    print("Command: " + cmd_str)
-    print("Directory: " + cwd)
-    
+def handle_result(cmd, cwd, cmd_success_msg=None, res=None):
+    stdout = ''
+    stderr = ''
     if res is not None:
         stdout = res.stdout
         stderr = res.stderr
     
     if len(stderr) > 0:
         if 'canberra-gtk-module' in stderr: # ignore, should still work properly
+            print("Command: " + str(cmd) +  
+                '\nDirectory: ' + cwd +
+                '\nCommand started.\n'
+            )
             return True
-        print("Error: " + stderr)
+        print("Command: " + str(cmd) +  
+            '\nDirectory: ' + cwd +
+            '\nError: ' + stderr + '\n'
+        )
         
         # https://github.com/ros2/ros2/issues/1406
         print('\nNOTE - If received the following error:')
@@ -79,10 +105,17 @@ def handle_result(cmd, cwd, res=None, stdout='', stderr=''):
         print('Run: "unset GTK_PATH" command first\n\n')
         return False
     else:
-        if len(stdout) > 0:
-            print("Message: " + stdout)
-        else:
-            print("Success!\n")
+        if cmd_success_msg is not None and \
+            cmd_success_msg not in stdout:
+            print("Command: " + str(cmd) +  
+                '\nDirectory: ' + cwd +
+                '\nCOMMAND FAILED\n'
+            )
+            return False
+        print("Command: " + str(cmd) +  
+            '\nDirectory: ' + cwd +
+            '\nCommand completed successfully!\n'
+        )
         return True
 
     
@@ -108,22 +141,21 @@ def run_command_in_terminal(cmd, cwd, cmd_success_msg):
         capture_output=True, \
         text=True
     )
-    if not handle_result(cmd, cwd, res):
-        return False
+    if not handle_result(cmd, cwd, None, res):
+        return False 
     
-    time.sleep(5) # wait for write to commence 
-    
+    # wait for terminal to open and begin stdout write
+    time.sleep(MAX_TERMINAL_LAUNCH_SECONDS) 
     
     file = "/tmp/" + filename
     
-    # wait for writes to complete
     seconds_since_last_write = 0
-    while seconds_since_last_write <= 2:
+    # continuously check file mod time until we are sure cmd output has been fully written
+    while seconds_since_last_write <= MAX_CONSECUTIVE_FWRITES_SECONDS:
         try:
             mod_time = datetime.fromtimestamp(os.path.getmtime(file))
             current_time = datetime.now()
             seconds_since_last_write = int((current_time - mod_time).total_seconds())
-            
         except FileNotFoundError:
             # File doesn't exist yet; wait and try again
             time.sleep(1) 
@@ -132,23 +164,51 @@ def run_command_in_terminal(cmd, cwd, cmd_success_msg):
     with open(file, "r") as file:
         contents = file.read()
         if cmd_success_msg in contents:
-            print('Success!\n')
+            print("Command: " + str(cmd) +  
+                  '\nDirectory: ' + cwd +
+                  '\nCommand completed successfully!\n'
+            )
             return True
-        else:  
+        else:
+            print("Command: " + str(cmd) +  
+                '\nDirectory: ' + cwd +
+                '\nFILE READ ERR!\n'
+            )
+            #print(contents)
             return False
 
 # runs specified command (cmd) in the current working directory (cwd) - takes little time to complete
-def run_command_no_terminal(cmd, cwd):
+def run_command_no_terminal(cmd, cwd, cmd_success_msg=None):
     # runs command without creating a pipe for monitoring process
     res = subprocess.run(cmd, \
         cwd=cwd, \
         capture_output=True, \
         text=True
     )
-    return handle_result(cmd, cwd, res=res)
+    return handle_result(cmd, cwd, cmd_success_msg, res=res)
+
+def run_and_populate_mysql():
+    print('Running and populating MySQL database\n')
+    
+    run_mysql = True
+    if run_command_no_terminal(COMMANDS['DOCKER_PS'], '.', 'mysql:8-oracle'):
+        run_mysql = False
+    
+    if run_mysql:
+        if not run_command_no_terminal(COMMANDS['DOCKER_RUN_MYSQL'], '.'):
+            return False
+    
+        # Ensure MySQL is up after running
+        if not run_command_no_terminal(COMMANDS['DOCKER_PS'], '.', 'mysql:8-oracle'):
+            return False
+     
+    # TODO: Populate initial SQL
+    
+    
+    return True
 
 def build_and_run_backend():
-    print('Building & running Java backend')
+    print('Building & running Java backend\n')
     
     if not run_command_no_terminal(COMMANDS['CLEAN_COMPILE'], SPRING_API_PATH):
         return False
@@ -162,7 +222,7 @@ def build_and_run_backend():
     return True
 
 def run_react_client_with_install():
-    print('Launching React client')
+    print('Launching React client\n')
 
     if not run_command_in_terminal(COMMANDS['NPM_INSTALL'], REACT_CLIENT_PATH, 'up to date,'):
         return False
@@ -179,18 +239,27 @@ if __name__ == '__main__':
         print('Setup failed.')
         raise SystemExit(1)
     
-    # TODO: run each of the following as a separate process to speed-up
     
-    # TODO: method to run MySQL Docker image
+    # Threads are efficient for concurrent I/O based tasks (writing files)
+    with ThreadPoolExecutor() as executor:
+        run_mysql_future = executor.submit(run_and_populate_mysql)
+        run_backend_future = executor.submit(build_and_run_backend)
+        run_react_future = executor.submit(run_react_client_with_install)
     
-    # TODO: method to populate MySQL with init.sql data
-    
-    if not build_and_run_backend():
-        print('Java backend failed to launch.')
+    # blocking call - halts execution until result returns here
+    mysql_valid_result = run_mysql_future.result() 
+    if not mysql_valid_result:
+        print('MySQL launch & populate task failed.\n')
+        raise SystemExit(1)
+
+    backend_valid_result = run_backend_future.result() 
+    if not backend_valid_result:
+        print('Java backend build & launch task failed.\n')
         raise SystemExit(1)
     
-    # if not run_react_client_with_install():
-    #     print('React client failed to launch.')
-    #     raise SystemExit(1)
+    react_valid_result = run_react_future.result() 
+    if not react_valid_result:
+        print('React client install & launch task failed.n')
+        raise SystemExit(1)
     
     print('Blockheads is up and running!')
